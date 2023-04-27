1ffb89c046926f8a1952cacaddcc08481f3a7789
ChanRithy27 <chanrithy27@icloud.com>
2023-04-27 16:12:22 -0700

    Progress on assignments

--- server.py
+++ server.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route('/play')
 def start_playground():
-    return render_template("index.html")
+    return render_template("index.html", x=3, color= "#9fc5f8")
+
+@app.route('/play/<int:x>')
+def display_boxes(x):
+    return render_template("index.html", x = x, color= "#9fc5f8")
 
 if __name__=='__main__':
     app.run(debug=True, host='localhost', port=8000)
